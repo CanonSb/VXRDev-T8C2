f0f23a841ae184469d671d4aa14d9d8b21fa8ab1
samshamrock12 <samshamrock12@gmail.com>
2024-10-23 00:11:25 -0500

    Added in scenes for victory and game over conditions
    Notably I did not add conditions to the main game to move into these scenes yet.

---
+++ Assets/Scripts/RestartController.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+public class RestartController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void RestartGame()
+    {
+        StartCoroutine(SwitchScenes());
+    }
+
+
+    public IEnumerator SwitchScenes()
+    {
+        // Disable sign text
+        GameObject.FindGameObjectWithTag("signText").SetActive(false);
+        // Start the fog lerp coroutine and wait for it to finish
+        yield return StartCoroutine(LerpFogDensity(1f));
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    // Coroutine to lerp fog density
+    private IEnumerator LerpFogDensity(float endDensity, float duration = 1f)
+    {
+        float timeElapsed = 0f;
+        float startDensity = RenderSettings.fogDensity;
+
+        while (timeElapsed < duration)
+        {
+            timeElapsed += Time.deltaTime;
+            float normalizedTime = Mathf.Clamp01(timeElapsed / duration);
+
+            RenderSettings.fogDensity = Mathf.Lerp(startDensity, endDensity, normalizedTime);
+            yield return null;
+        }
+
+        // Ensure the final fog density is set
+        RenderSettings.fogDensity = endDensity;
+    }
+}
